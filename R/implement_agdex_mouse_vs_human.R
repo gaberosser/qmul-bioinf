d4f564547e7b80ee8b616ca57093f76c27da3258
Gabriel Rosser <gabriel.rosser@gmail.com>
2016-10-17 17:55:24 +0100

    Continued work with homolog table generation in Python. Continued to work towards running AGDEX in R. Added CSV version of the SB microarray data.

--- R/implement_agdex_mouse_vs_human.R
+++ R/implement_agdex_mouse_vs_human.R
@@ -5,18 +5,122 @@
 #library("pd.hugene.1.1.st.v1")
 
 library(annotationTools)
+library(biomaRt)
 
-musID = 10090
+# *Should* be able to automate the process of finding homologous probesets in the two microarrays
+# using the code found here: https://mikedewar.wordpress.com/2010/05/14/generating-homologues-using-biomart/
+# Unfortunately, the probeset is not defined ni biomaRt.
 
-dataDir <- "/home/gabriel/Dropbox/research/qmul/data/sleeping_beauty_mouse_screen/"
-annoDir <- "/home/gabriel/Dropbox/research/qmul/data/microarray_annotation/"
+mart <- useMart(
+  "ensembl",
+  dataset = "hsapiens_gene_ensembl"
+)
+martAttrs <- listAttributes(mart)
+martAttrs[grep('affy', martAttrs$name),]  # see - doesn't have our microarray
 
-homologeneFile <- file.path(dataDir, "homologene.data")
-moAnnoFile <- file.path(annoDir, "MoGene-1_1-st-v1.na36.mm10.probeset.csv.gz")
-huAnnoFile <- file.path(annoDir, "HuGene-1_1-st-v1.na36.hg19.probeset.csv.gz")
+# gen_hs2mm <- function(affyids){
+#   ensembl_hs <- useMart(
+#     "ensembl",
+#     dataset = "hsapiens_gene_ensembl"
+#   )
+#   hs2mm_filters <- c(
+#     "affy_hugene_1_1_st_v1",
+#     "with_mmusculus_homolog"
+#   )
+#   hs2mm_gene_atts <- c(
+#     "affy_hugene_1_1_st_v1",  ## NUH-UH
+#     "ensembl_gene_id"
+#   )
+#   hs2mm_homo_atts <- c(
+#     "ensembl_gene_id",
+#     "mouse_ensembl_gene"
+#   )
+#   # the names in these lists are arbitrary
+#   hs2mm_value = list(
+#     affyid=affyids,
+#     with_homolog=TRUE
+#   )
+#   # get the human genes and mouse orthologues
+#   hs2mm_gene <- getBM(
+#     attributes = hs2mm_gene_atts,
+#     filters = hs2mm_filters,
+#     value = hs2mm_value,
+#     mart = ensembl_hs
+#   )
+#   
+#   hs2mm_homo <- getBM(
+#     attributes = hs2mm_homo_atts,
+#     filters = hs2mm_filters,
+#     value = hs2mm_value,
+#     mart = ensembl_hs
+#   )
+#   # merge the two lists!
+#   hs2mm <- merge(hs2mm_gene,hs2mm_homo)
+# }
+# 
+# gen_mm2hs <- function(affyids){
+#   ensembl_mm <- useMart("ensembl",
+#                         dataset = "mmusculus_gene_ensembl")
+#   mm2hs_filters <- c(
+#     "affy_mogene_1_1_st_v1",  ## NUH-UH
+#     "with_hsapiens_homolog"
+#   )
+#   mm2hs_gene_atts <- c(
+#     "affy_mogene_1_0_st_v1",
+#     "ensembl_gene_id"
+#   )
+#   mm2hs_homo_atts <- c(
+#     "ensembl_gene_id",
+#     "human_ensembl_gene"
+#   )
+#   # the names in these lists are arbitrary
+#   mm2hs_value = list(
+#     affyids=affyids,
+#     with_homolog=TRUE
+#   )
+#   # get the mouse genes and human orthologues
+#   mm2hs_gene <- getBM(
+#     attributes = mm2hs_gene_atts ,
+#     filters = mm2hs_filters,
+#     value = mm2hs_value,
+#     mart = ensembl_mm
+#   )
+#   mm2hs_homo <- getBM(
+#     attributes = mm2hs_homo_atts,
+#     filters = mm2hs_filters,
+#     value = mm2hs_value,
+#     mart = ensembl_mm
+#   )
+#   mm2hs <- merge(mm2hs_gene,mm2hs_homo)
+#   
+# }
 
-huAnno <- read.csv(huAnnoFile, colClasses='character', comment.char='#')
-moAnno <- read.csv(moAnnoFile, colClasses='character', comment.char='#')
-homologene <- read.delim(homologeneFile, header=FALSE)
+muID = 10090
+huID = 9606
 
-orthoTable<-ps2ps(huAnno, moAnno, homologene, 10090)  # 10090: mus musculus
+dataDir <- "../data/sleeping_beauty_mouse_screen/"
+#annoDir <- "/home/gabriel/Dropbox/research/qmul/data/microarray_annotation/"
+annoDir <- "../## NUH-UHdata/microarray_annotation/"
+
+homologeneFile <- file.path("../data/homologene", "homologene.data")
+moAnnoFile <- file.path(annoDir, "MoGene-1_1-st-v1.na36.mm10.transcript.csv.gz")
+huAnnoFile <- file.path(annoDir, "HuGene-1_1-st-v1.na36.hg19.transcript.csv.gz")
+exprFile <- file.path(dataDir, "Dubuc_BMi1_Tg Expression profile.csv")
+
+# huAnno <- read.csv(huAnnoFile, colClasses='character', comment.char='#')
+# moAnno <- read.csv(moAnnoFile, colClasses='character', comment.char='#')
+# homologene <- read.delim(homologeneFile, header=FALSE)
+
+# load raw expression data
+expr <- read.csv(exprFile, header = TRUE, skip = 1, row.names = 1)
+
+# assay data: the raw expression counts, as a matrix F (# probe sets) x S (# samples)
+assayData <- as.matrix(expr[setdiff(colnames(expr), c('Gene.Symbol', 'mRNA.Accession'))])
+
+# pheno data: the conditions, as a table S x V (# covariates)
+chd7Status <- t(read.csv(exprFile, nrows = 1, header = FALSE))[2:9]
+chd7Status <- data.frame(chd7Status, row.names = colnames(assayData))
+pData <- table(rownames(chd7Status), chd7Status$chd7Status)
+
+# orthoTable<-ps2ps(huAnno, moAnno, homologene, 10090)  # 10090: mus musculus
+
--- scripts/agdex_mouse_human_mb_microarray/generate_ortholog_table.py
+++ scripts/agdex_mouse_human_mb_microarray/generate_ortholog_table.py
@@ -30,12 +30,18 @@ with gzip.open(hu_anno_file, 'rb') as f:
     l = '#'
     while l[0] == '#':
         l = f.readline()
-    c = csv.DictReader(f, fieldnames=l.split(','), delimiter=',')
+    fieldnames = l.replace('"', '').replace('\n', '').split(',')
+    c = csv.DictReader(f, fieldnames=fieldnames, delimiter=',')
     hu_anno = list(c)
 
-## TODO: add index
 hu_anno = pd.DataFrame(hu_anno)
+hu_anno.set_index('probeset_id', inplace=True)
+hu_anno.replace(to_replace='---', value=np.nan, inplace=True)
+
 mo_anno = pd.read_csv(mo_anno_file, comment='#', header=0)
+mo_anno.set_index('probeset_id', inplace=True)
+mo_anno.replace(to_replace='---', value=np.nan, inplace=True)
+
 homologs = pd.read_csv(homolog_file, header=None, names=homolog_header, sep='\t')
 
 # map human <-> mouse
@@ -44,8 +50,35 @@ hu_hid = homologs.loc[homologs.taxonomy_id == human_tid, 'hid']
 mo_hid = homologs.loc[homologs.taxonomy_id == mouse_tid, 'hid']
 
 # dedupe independently: no orthologs with the same hid
-hu_hid = hu_hid[~hu_hid.duplicated()]
-mo_hid = mo_hid[~mo_hid.duplicated()]
+# hu_hid = hu_hid[~hu_hid.duplicated()]
+# mo_hid = mo_hid[~mo_hid.duplicated()]
 
 # reduce to common pool
 joint_hid = np.intersect1d(hu_hid.values, mo_hid.values)
+
+# only keep 1:1 mappings (but quantify this loss of m2m mappings)
+hu_mappings = homologs.loc[(homologs.taxonomy_id == human_tid) & (homologs.hid.isin(joint_hid))]
+hm_all = hu_mappings.shape[0]
+hu_mappings = hu_mappings.loc[~hu_mappings.hid.duplicated()]
+hm_uniq = hu_mappings.shape[0]
+
+mo_mappings = homologs.loc[(homologs.taxonomy_id == mouse_tid) & (homologs.hid.isin(joint_hid))]
+mo_all = mo_mappings.shape[0]
+mo_mappings = mo_mappings.loc[~mo_mappings.hid.duplicated()]
+mo_uniq = mo_mappings.shape[0]
+
+# use gene symbols to map between homologene and annotations
+
+def get_gene_symbol(x):
+    return x.split(' // ')[1]
+
+hu_anno.loc[~hu_anno.gene_assignment.isnull(), 'gene_symbol'] = \
+    hu_anno.gene_assignment.loc[~hu_anno.gene_assignment.isnull()].apply(get_gene_symbol)
+
+mo_anno.loc[~mo_anno.gene_assignment.isnull(), 'gene_symbol'] = \
+    mo_anno.gene_assignment.loc[~mo_anno.gene_assignment.isnull()].apply(get_gene_symbol)
+
+# lookup
+
+hu_anno_match = hu_anno.loc[hu_anno.gene_symbol.isin(hu_mappings.gene_symbol.values)]
+mo_anno_match = mo_anno.loc[mo_anno.gene_symbol.isin(mo_mappings.gene_symbol.values)]
