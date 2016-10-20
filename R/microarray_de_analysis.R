9d7aa6dfca6d41383ee9fb84197c943cf2677bf4
Gabriel Rosser <gabriel.rosser@gmail.com>
2016-10-20 16:11:02 +0100

    Updated data sources to include easily loaded RData file. Testing out DE using limma

--- R/microarray_de_analysis.R
+++ R/microarray_de_analysis.R
@@ -2,9 +2,63 @@
 # biocLite("GEOquery")
 library(Biobase)
 library(GEOquery)
+library("limma")
 
-# load the sample CSV table
-samples <- read.csv("../data/microarray_GSE28192/sources.csv")
+dataDir <- '../data/'
+microarrayDir <- file.path(dataDir, 'microarray_GSE28192')
+microarrayFile <- file.path(microarrayDir, 'microarray.RData')
+
+loadCsv <- function (x) {
+  read.csv(x, sep='\t', header = 1, row.names = 1)
+}
+
+
+load_from_raw <- function() {
+  # load the sample CSV table
+  samples <- read.csv(file.path(microarrayDir, "sources.csv"), stringsAsFactors = FALSE)
+  
+  # load the expression data
+  fileList <- apply(samples[2], 1, function(x) file.path(microarrayDir, paste(x, '.gz', sep='')))
+  # load first file
+  d <- loadCsv(fileList[1])
+  
+  expr <- data.frame(row.names = rownames(d))
+  pVal <- data.frame(row.names = rownames(d))
+  expr[samples[1, 2]] <- d[, 1]
+  pVal[samples[1, 2]] <- d[["Detection.Pval"]]
+  
+  for (i in 2:length(fileList)) {
+    d <- loadCsv(fileList[i])
+    expr[samples[i, 2]] <- d[, 1]
+    pVal[samples[i, 2]] <- d[["Detection.Pval"]]
+  }
+  
+  save(list=c("expr", "pVal", "samples"), file=microarrayFile, compress=TRUE)
+  return(
+    list("expr" = expr, "pVal" = pVal, "samples" = samples)
+  )
+}
+
+
+load(microarrayFile)
+
+
+healthySampleNames = c(
+  'NT1197',
+  'NCb1',
+  'NCb2',
+  'A911105',
+  'A508112',
+  'A508285'
+)
+
+# setup design matrix
+es1 <- factor(samples$northcott.classification)
+es2 <- factor(sapply(samples[,2], function(x) substr(x, nchar(x) - 1, nchar(x)) == '-R'))
+
+X <- model.matrix(~0 + es1 + es2)
+# Does this do what we want??
+lm.fit(expr, X)
 
 # gse <- getGEO('GSE28192', destdir='./tmp-download')
 gse <- getGEO(filename='tmp-download/GSE28192_series_matrix.txt.gz')
