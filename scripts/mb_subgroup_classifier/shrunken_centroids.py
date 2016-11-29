da4fb1644a263b6a996ce3e091fd14d6f1830b7f
Gabriel Rosser <g.rosser@qmul.ac.uk>
2016-11-29 17:34:35 +0000

    Partially functional shrunken centroid classifier code. TODO: cross-validation seems to be failing - classifier just gets better as the shrinkage is increased. That suggests that a single gene is better than a combination - surely not? Start by looking at which feature remains at the end. Also check why it even works with no features - the discriminant score is NaN, so it should just error?

---
+++ scripts/mb_subgroup_classifier/shrunken_centroids.py
@@ -0,0 +1,213 @@
+import numpy as np
+import pandas as pd
+from load_data import microarray_data, allen_human_brain_atlas
+import collections
+import operator
+import log
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+logger = log.get_console_logger(__name__)
+
+
+def soft_threshold(x, delta):
+    """
+    Apply soft thresholding to the input array x
+    :param x: Array of any dimension
+    :param delta: The absolute quantity to subtract from each entry in x
+    :return: A new array of the same shape as x
+    """
+    u = np.abs(x) - delta
+    u[u < 0] = 0.
+    u *= np.sign(x)
+    return u
+
+
+def tile_series(x, k, index=None, columns=None):
+    """
+    Tile the supplied series to create a DataFrame with k identical columns
+    :param x: Input data
+    :param k: Number of times to tile
+    :return:
+    """
+    a = pd.DataFrame(
+        np.tile(x, (k, 1)).transpose(),
+        columns=columns,
+        index=index
+    )
+    return a
+
+
+class NearestCentroidClassifier(object):
+    def __init__(self, data, labels, flat_prior=False):
+        """
+        Statistical model in which the data are described as a collection of centroids in p-dimensional space.
+        :param data: The input data, array of dim P x N, where N is the number of samples and P is the number of features.
+        :param labels: 1D array of length P containing the group labels
+        :param flat_prior: If True, the prior probabilities for group membership are assumed to be uniform. Otherwise
+         (the default) the group membership in the input data are assumed to reflect the underlying probability.
+        """
+        self.data = pd.DataFrame(data)
+        self.p, self.n = self.data.shape
+        self.labels = labels
+        self.label_idx, self.labels_ = pd.Series(labels, index=self.data.columns).factorize()
+        self.k = len(self.labels_)
+
+        # compute global and group-specific centroids and standard errors
+        self.global_centroid = self.data.mean(axis=1)
+        self.class_centroids = X.groupby(self.labels, axis=1).mean().loc[:, self.labels_]
+        self.n_class = X.groupby(self.labels, axis=1).size().loc[self.labels_]
+        self.m_k = np.sqrt(1 / self.n_class + 1 / float(self.n))
+
+        if flat_prior:
+            self.prior_prob = pd.Series(np.ones(self.k) / float(self.k), index=self.labels_)
+        else:
+            self.prior_prob = self.n_class / self.n_class.sum()
+
+        # pooled within-class stdev
+        y = self.data.copy()
+        for i, l in enumerate(self.labels_):
+            y.loc[:, self.labels == l] = y.loc[:, self.labels == l].subtract(self.class_centroids.loc[:, l], axis=0).values
+        s_i_sq = (y ** 2).sum(axis=1) / (self.n - self.k)
+        self.global_stdev = s_i_sq ** 0.5
+
+        # s_0: median value of s over all genes
+        self.s0 = self.global_stdev.median()
+
+        # linear discriminant
+        d_num = self.class_centroids.subtract(self.global_centroid, axis=0)
+
+        # tiled dataframe (P x K) for convenience
+        a = tile_series(self.global_stdev, self.k, columns=self.labels_, index=self.data.index)
+
+        self.s_eff = a + self.s0
+        self.d_den = self.s_eff.multiply(self.m_k)
+        self.d_ik = d_num / self.d_den
+
+        # placeholders for shrinkage operations
+        self.d_ik_shrink = self.d_ik
+        self.class_centroids_shrink = self.class_centroids
+        self.delta = None
+
+    def shrink_centroids(self, delta):
+        """
+        Shrink the class centroids by the absolute quantity delta using soft thresholding.
+        This has the effect of setting self.class_centroids_shrink and self.d_ik_shrink
+        :param delta: Absolute size of shrinkage, applied to linear discriminants
+        :return: None
+        """
+        self.delta = delta
+        self.d_ik_shrink = soft_threshold(self.d_ik, delta=delta)
+        self.class_centroids_shrink = (self.d_den * self.d_ik_shrink).add(self.global_centroid, axis=0)
+        if self.num_nonzero_features == 0:
+            logger.warning("No features remain after shrinking. Attempting to classify will result in an error.")
+
+    @property
+    def num_nonzero_features(self):
+        """
+        :return: The number of features for which at least one of the subgroups has a non-zero linear discriminant.
+        Features that are all zeros do not have any influence over the inferred classification.
+        """
+        return (self.d_ik_shrink != 0.).any(axis=1).sum()
+
+    def discriminant_score(self, vec):
+        """
+        Compute the discriminant score for an arbitrary vector of length P for each of the classes
+        :param vec:
+        :return: Array length K
+        """
+        if self.num_nonzero_features == 0:
+            logger.error("No features remaining. Reduce shrinkage parameter.")
+            raise ValueError("No features remaining. Reduce shrinkage parameter.")
+        num = self.class_centroids_shrink.subtract(vec, axis=0)
+        den = self.s_eff
+        return ((num / den) ** 2).sum(axis=0) - 2 * np.log(self.prior_prob)
+
+    def class_probabilities(self, vec):
+        dk = self.discriminant_score(vec)
+        # if the discriminant scores are high, all of the exponential functions could give a numerical 0.
+        # therefore subtract the minimum value to guarantee at least one
+        # this does not affect the RATIO of the values, which is what is measured.
+        dk -= dk.min()
+        gk = np.exp(-0.5 * dk)
+        return gk / gk.sum()
+
+    def classify(self, vec, int_index=False):
+        """
+        Compute the classification of the P-length array vec
+        :param vec:
+        :param int_index: If True, return the integer that indexes the label, else return the label string itself.
+        :return:
+        """
+        dk = self.discriminant_score(vec)
+        if int_index:
+            dk.index = range(self.k)
+        return dk.argmin()
+
+    def classify_and_assess(self, mat, true_labels):
+        """
+        Run the classification on the samples in the input matrix
+        :param mat: P x M DataFrame, where M is the number of test samples
+        :param true_labels: The true labels (length M). These are used only after classification to assess the result.
+        :return: inferred_labels, n_correct, n_incorrect
+        """
+        m = mat.columns.size
+        res = pd.Series([self.classify(mat.iloc[:, i]) for i in range(m)], index=mat.columns)
+        n_correct = (res == true_labels).sum()
+        n_incorrect = m - n_correct
+        return res, n_correct, n_incorrect
+
+
+
+if __name__ == '__main__':
+    res, meta = microarray_data.load_annotated_microarray_gse37382(index_field='gene_symbol')
+    idx, labels = meta.subgroup.factorize()
+    X = res  # genes on rows, samples on cols
+
+    # define shrinkage delta values
+    deltas = np.linspace(0., 2., 50)
+
+    # split test-training, maintaining proportion of classes
+    k = 10
+    training = collections.defaultdict(list)
+    test = collections.defaultdict(list)
+
+    # pre-compute the permuted indices for each class
+    train_idx = []
+    test_idx = []
+    perm_ind = {}
+    chunks = collections.defaultdict(list)
+    for i, l in enumerate(labels):
+        subgrp_ind = np.where(idx == i)[0]
+        nl = (idx == i).sum()
+        # random order
+        pix = np.random.permutation(nl)
+        perm_ind = subgrp_ind[pix]
+        ch = np.array_split(perm_ind, indices_or_sections=k)
+        for j in range(k):
+            chunks[j].extend(ch[j])
+
+    # Xv
+    train_idx = []
+    test_idx = []
+    n_corr = pd.DataFrame(index=range(k), columns=deltas)
+    clas = pd.DataFrame(index=range(k), columns=deltas)
+    for j in range(k):
+        te = chunks[j]
+        tr = reduce(operator.add, [chunks[z] for z in set(range(k)).difference([j])])
+        test_idx.append(te)
+        train_idx.append(tr)
+        obj = NearestCentroidClassifier(data=X.iloc[:, tr], labels=meta.subgroup.iloc[tr])
+        for d in deltas:
+            obj.shrink_centroids(d)
+            c, nc, ni = obj.classify_and_assess(X.iloc[:, te], true_labels=meta.subgroup.iloc[te])
+            n_corr.loc[j, d] = nc
+            clas.loc[j, d] = c.values
+
+
+    obj = NearestCentroidClassifier(data=X, labels=meta.subgroup)
+
+    n_remain = pd.Series(index=deltas)
+    for d in deltas:
+        obj.shrink_centroids(d)
+        n_remain.loc[d] = obj.num_nonzero_features
