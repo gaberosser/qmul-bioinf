ff1a305659c4b0dc0736e80dc5f2694bebe4d079
Gabriel Rosser <g.rosser@qmul.ac.uk>
2017-01-16 16:41:54 +0000

    Added SAM->BAM to the align by hisat2 script. TODO: test this. Added featureCounts script, which is working fine. Added a generic meta loader for series matrix files.  It's not perfect - outputs need a bit of manual tweaking, but it's a good start.

---
+++ scripts/featurecounts_counting.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import subprocess
+import os
+import sys
+import re
+import csv
+from gzip import GzipFile
+from log import get_file_logger, get_console_logger
+logger = get_console_logger(__name__)
+
+
+FEATURECOUNTS_CMD = 'featureCounts'
+
+
+
+if __name__ == "__main__":
+    """
+    Usage: featurecounts_counting.py path_to_bams path_to_GTF path_to_output passed_to_featureCounts
+    If no parameters are aupplied to pass to featureCounts, a default set are used that work for
+    Illumina paired end RNA-Seq.
+    """
+    read_dir = sys.argv[1]
+    ref_fn = sys.argv[2]
+    out_dir = sys.argv[3]
+    fc_args = sys.argv[4:]
+
+    if len(fc_args) == 0:
+        fc_args = [
+            '-p',  # paired end
+            '-B',  # only count read pairs that are both successfully aligned
+            '-P',  # Check validity of paired-end distance
+            '-d', '50',  # Minimum template length
+            '-D', '600',  # Maximum template length
+            '-s', '2',  # Reverse-stranded
+            '-T', '12',  # Parallel threads
+        ]
+
+
+    # put global log output into the output directory
+    log_fn = os.path.join(out_dir, 'featureCounts.log')
+
+    if not os.path.isdir(read_dir):
+        raise ValueError("Could not find specified read directory %s" % read_dir)
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+    outfile = os.path.join(out_dir, 'counts.txt')
+    logger = get_file_logger(__name__, log_fn)
+
+    # BAM file discovery
+    rr = re.compile(r'\.bam$', flags=re.IGNORECASE)
+    flist = [os.path.join(read_dir, t) for t in os.listdir(read_dir) if re.search(rr, t)]
+    cmd = [
+        FEATURECOUNTS_CMD,
+        '-a', ref_fn,
+        '-o', outfile,
+    ] + fc_args + flist
+    stderr_fn = os.path.join(out_dir, 'featureCounts.stderr')
+    logger.info("Running featureCounts, piping stderr > %s", stderr_fn)
+    logger.info("%s", ' '.join(cmd))
+    with open(stderr_fn, 'wb') as stderr:
+        subprocess.call(cmd, stderr=stderr)
--- scripts/hisat2_alignment.py
+++ scripts/hisat2_alignment.py
@@ -2,10 +2,20 @@
 import subprocess
 import os
 import sys
+import re
 import csv
-from log import get_file_logger
+from gzip import GzipFile
+from Bio import SeqIO
+from log import get_file_logger, get_console_logger
+import numpy as np
+from StringIO import StringIO
+logger = get_console_logger(__name__)
 
 INSERT_SH = 'scripts/bash/insert_size_from_bam.sh'
+HISAT_CMD = 'hisat2'
+
+
+# TODO: add proper argument parsing, which is especially useful for --threads, which is used in more than one place.
 
 
 def main(read_dir, ref_dir, prealign_dir, *args):
@@ -17,43 +27,111 @@ def main(read_dir, ref_dir, prealign_dir, *args):
         raise ValueError("Could not find specified prealignment directory %s" % prealign_dir)
 
 
-def get_read_length(read_fn):
+def get_read_length(read_fn, n_read=10000):
     """
-    Get the read length from the .fastgz file
-    :param read_fn:
+    Get the read length from the .fastq.gz file
+    :param read_fn: .fastq.gz file
     :return:
     """
-    pass
-
+    with GzipFile(read_fn, mode='rb') as f:
+        h = SeqIO.QualityIO.FastqGeneralIterator(f)
+        i = 0
+        l = []
+        while i < n_read:
+            try:
+                t = h.next()
+                l.append(len(t[1]))
+                i += 1
+            except StopIteration:
+                logger.warning("Requested %d reads but reached the end of the file after %d", n_read, i)
+    return int(np.round(np.mean(l)))
 
 
 if __name__ == "__main__":
     """
-    Usage: hisat2_alignment.py path_to_reads path_to_bt2 path_to_insert_size.csv passed_to_hisat2
+    Usage: hisat2_alignment.py path_to_reads path_to_bt2 path_to_output passed_to_hisat2
     """
     read_dir = sys.argv[1]
-    ref_dir = sys.argv[2]
-    prealign_dir = sys.argv[3]
+    ref_fn = sys.argv[2]
+    out_dir = sys.argv[3]
     hisat_args = sys.argv[4:]
 
-    # put log output into the output directory
-    log_fn = os.path.join(prealign_dir, 'hisat2_alignment')
-
-    logger = get_file_logger(__name__, log_fn)
+    # put global log output into the output directory
+    log_fn = os.path.join(out_dir, 'hisat2_alignment')
 
     if not os.path.isdir(read_dir):
         raise ValueError("Could not find specified read directory %s" % read_dir)
-    if not os.path.isdir(ref_dir):
-        raise ValueError("Could not find specified reference directory %s" % ref_dir)
-    if not os.path.isfile(prealign_dir):
-        raise ValueError("Could not find specified prealignment directory %s" % prealign_dir)
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
 
-    ins_fn = os.path.join(prealign_dir, 'insert_size.csv')
-    if os.path.exists(ins_fn):
-        logger.info("Using existing insert sizes CSV %s", ins_fn)
-    else:
-        logger.info("Unable to find insert sizes CSV %s so will create now", ins_fn)
-        logger.error("The process of calling this from python is SLOW and I don't know why!")
-        with open(ins_fn, 'rb') as f:
-            subprocess.call([INSERT_SH, prealign_dir], stdout=)
+    logger = get_file_logger(__name__, log_fn)
+    
+    # fastq.gz file discovery
+    rr = re.compile(r'\.fastq\.gz$', flags=re.IGNORECASE)
+    flist = [t for t in os.listdir(read_dir) if re.search(rr, t)]
+    # check for existing output and identify pairs of files
+    fl = {}
+    for t in flist:
+        base = re.sub(r'_[12]\.fastq\.gz', '', t)
+        read_num = int(re.sub(r'\.fastq\.gz', '', t)[-1])
+        sam_out = os.path.join(out_dir, "%s.sam" % base)
+        # if SAM output file exists, log warning and skip
+        if os.path.isfile(sam_out):
+            logger.warn("File already exists: %s. Skipping.", sam_out)
+            continue
 
+        if base not in fl:
+            fl[base] = {
+                read_num: os.path.join(read_dir, t),
+                'sam_out': sam_out,
+            }
+        else:
+            fl[base][read_num] = os.path.join(read_dir, t)
+
+    # log the filelist
+    logger.info("Found %d fastq pairs: %s.", len(fl), ', '.join(fl.keys()))
+
+    # start the jobs
+    for base, d in fl.iteritems():
+        # create a temporary stderr file (StringIO doesn't work)
+        stderr_fn = os.path.join(out_dir, '%s.stderr' % base)
+        cmd = [HISAT_CMD] + hisat_args + [
+            '-x',
+            ref_fn,
+            '-1',
+            d[1],
+            '-2',
+            d[2],
+            '-S',
+            d['sam_out']
+        ]
+        logger.info("Running alignment on sample %s: %s", base, ' '.join(cmd))
+        logger.info("Sending stderr to temporary file %s", stderr_fn)
+        with open(stderr_fn, 'wb') as stderr:
+            try:
+                subprocess.call(cmd, stderr=stderr)
+            except Exception as exc:
+                logger.exception("Alignment failed.")
+        logger.info("Alignment complete")
+
+        # convert to BAM and delete the original SAM
+        cmd = ['samtools', 'view', '--threads', '8', '-b', d['sam_out']]
+        bam_out = re.sub(r'\.sam$', '\.bam', d['sam_out'])
+        logger.info("Converting %s -> %s", d['sam_out'], bam_out)
+        with open(bam_out, 'wb') as stdout:
+            logger.info("%s", ' '.join(cmd))
+            try:
+                subprocess.call(cmd, stdout=stdout)
+            except Exception as exc:
+                logger.exception("Conversion failed.")
+        logger.info("Conversion complete.")
+
+        # delete SAM file
+        cmd = ['rm', d['sam_out']]
+        logger.info("Deleting original SAM file %s", d['sam_out'])
+        try:
+            subprocess.call(cmd)
+        except Exception as exc:
+            logger.exception("Deletion failed.")
+
+        logger.info("All complete")
