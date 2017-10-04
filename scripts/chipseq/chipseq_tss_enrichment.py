6a5e777084038cdc16cf0ab02501e444a3f86811
Gabriel Rosser <gabriel.rosser@gmail.com>
2017-10-04 14:43:21 +0100

    Created script to convert gtf file into bed file of TSS +- 2kb. For chipseq purposes. Started working on a script to load samtools depth output and make traces.

---
+++ scripts/chipseq/chipseq_tss_enrichment.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import gzip
+import os
+import csv
+
+
+def coverage_reader(cov_fn, exclude=None):
+    if isinstance(exclude, str):
+        exclude = {exclude}
+    elif exclude is not None:
+        exclude = set(list(exclude))
+
+    with gzip.GzipFile(cov_fn, 'rb') as f:
+        c = csv.reader(f, delimiter='\t')
+        current_ch = None
+        current_res = []
+        current_count = 0
+        for line in c:
+            if exclude is not None and line[0] in exclude:
+                continue
+            if line[0] != current_ch:
+                if current_count > 0:
+                    yield (current_ch, np.array(current_res).astype(np.uint32))
+                # new chromosome
+                current_ch = line[0]
+                current_count = 0
+                print "Start reading depth for chromosome %s" % current_ch
+                current_res = []
+            current_res.append(line[1:])  # ints are more space efficient and useful
+            current_count += 1
+            if (current_count % 1000000) == 0:
+                print "Line %d" % current_count
+        # yield final result
+        yield (current_ch, np.array(current_res).astype(np.uint32))
+
+
+if __name__ == "__main__":
+    distance = 2000
+    cov_fn = '/home/gabriel/Documents/qmul_data/samtools_depth_tss_pm_2000.cov.gz'
+    gene_bed_fn = '/home/gabriel/Documents/qmul_data/tss_pm_2000.bed'
+    samples = [
+        ("GBM026", "H3K27me3"),
+        ("GBM026", "H3K36me3"),
+        ("Icb1299", "H3K4me3"),
+        ("Icb1299", "H3K27me3"),
+        ("Icb1299", "BMI1"),
+        ("Icb1299", "CHD7"),
+        ("Dura026_NSC", "H3K4me3"),
+        ("Dura026_NSC", "H3K27me3"),
+        ("Dura026_NSC", "H3K36me3"),
+        ("GBM026", "H3K4me3"),
+    ]
+
+    # load gene bed file
+    bed = []
+    with open(gene_bed_fn, 'rb') as f:
+        c = csv.reader(f, delimiter='\t')
+        for line in c:
+            bed.append([line[0], int(line[1]), int(line[2]), line[3]])
+
+    # exclusion list of non-standard chroms
+    excl_chroms = [t for t in sorted(set([t[0] for t in bed])) if t[0] in ('G', 'K')]
+
+    for chrom, depth in coverage_reader(cov_fn=cov_fn, exclude=excl_chroms):
+
+        this_trace = np.zeros((2 * distance + 1, len(samples)))
+        # get the coverage for each sample in each tss region
+        for t in bed:
+            if t[0] == chrom:
+                ix = t[1] <= depth[:, 0] <= t[2]
+                # this_trace +=
---
+++ scripts/chipseq/tss_from_gtf.py
@@ -0,0 +1,63 @@
+import gzip
+import os
+import re
+import csv
+
+
+if __name__ == "__main__":
+    LIMIT = int(1e12)
+    SOURCES = {'ensembl', 'havana', 'ensembl_havana'}
+    distance = 2000 # distance from TSS to include
+    fn = '/home/gabriel/Documents/qmul_data/Homo_sapiens.GRCh38.87.gtf.gz'
+    fn_out = '/home/gabriel/Documents/qmul_data/tss_pm_%d.bed' % distance
+
+    res = []
+    bed = []
+
+    with gzip.open(fn, 'rb') as f:
+        c = csv.reader(f, delimiter='\t')
+        line_no = 1
+        for row in c:
+            if len(row) == 1:
+                continue
+            if line_no > LIMIT:
+                break
+            chr = row[0]
+            src = row[1]
+            typ = row[2]
+            start = int(row[3])
+            stop = int(row[4])
+            strand = row[6]
+            blk = row[8].split('; ')
+
+            if strand == '+':
+                start = int(row[3])
+                stop = int(row[4])
+            elif strand == '-':
+                start = int(row[4])
+                stop = int(row[3])
+
+            attr = dict([t.split(' ') for t in blk])
+
+            if src in SOURCES \
+                    and typ == 'exon' \
+                    and attr.get('exon_number') == '"1"'\
+                    and attr.get('gene_biotype') == '"protein_coding"':
+                attr['chr'] = chr
+                attr['source'] = src
+                attr['type'] = typ
+                attr['start'] = start
+                attr['stop'] = stop
+                attr['strand'] = strand
+                res.append(attr)
+
+                tss_minus = start - distance
+                tss_plus = start + distance
+
+                bed.append([chr, tss_minus, tss_plus, attr['transcript_name'].strip('"')])
+
+            line_no += 1
+
+    with open(fn_out, 'wb') as f:
+        c = csv.writer(f, delimiter='\t')
+        c.writerows(bed)
