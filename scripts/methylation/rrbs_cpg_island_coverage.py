339f394c5f397ea690a96e47dae00c5bdf2d2ad1
Gabriel Rosser <gabriel.rosser@gmail.com>
2018-01-28 10:11:48 +0000

    Modified RRBS script to enable command line specification of input file. Planning to use this on apocrita

--- scripts/methylation/rrbs_cpg_island_coverage.py
+++ scripts/methylation/rrbs_cpg_island_coverage.py
@@ -3,8 +3,10 @@ import os
 import re
 import pandas as pd
 import multiprocessing as mp
+import argparse
 import json
 import pysam
+import datetime
 from StringIO import StringIO
 
 # add root of project dir to the path
@@ -12,7 +14,10 @@ sys.path.append(os.path.dirname(__file__) + '/../../')
 from settings import DATA_DIR_NON_GIT, GIT_LFS_DATA_DIR
 from utils import genomics, output, log
 
-clogger = log.get_console_logger(__name__)
+log_dir = os.path.join(os.environ['HOME'], 'log')
+now_str = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+flogger = log.get_file_logger("rrbs_cpg_island_coverage", os.path.join(log_dir, "rrbs_cpg_island_coverage"))
+# clogger = log.get_console_logger(__name__)
 
 
 def get_one_coverage(bam_fn, region, region_pad=500, n_perm=100):
@@ -24,37 +29,36 @@ def get_one_coverage(bam_fn, region, region_pad=500, n_perm=100):
 
     # get the coverage over this region
     region_depth = pysam.depth(bam_fn, "-a", "-r", "%s:%d-%d" % (chrom, start, end))
-    # region_depth = pd.read_csv(StringIO(this_st_depth), sep='\t', header=None)
-    # region_depth.columns = ['chrom', 'coord', 'cov']
-
-    # this_st_depth = pysam.depth(bam_fn, "-r", "%s:%d-%d" % (chrom, start, end))
-    # if len(this_st_depth) > 0:
-    #     # more parsing is needed
-    #     region_depth = pd.read_csv(StringIO(this_st_depth), sep='\t', header=None)
-    #     region_depth.columns = ['chrom', 'coord', 'cov']
 
     for j in range(n_perm):
         chrom, start, end = genomics.random_genomic_interval(chrom_lengths, n_bp=end - start + 1)
         perm_depths[j] = pysam.depth(bam_fn, "-a", "-r", "%s:%d-%d" % (chrom, start, end))
-        # this_depth = pd.read_csv(StringIO(this_st_depth), sep='\t', header=None)
-        # this_depth.columns = ['chrom', 'coord', 'cov']
-        # perm_depths[j] = this_depth
-
-        # this_st_depth = pysam.depth(bam_fn, "-r", "%s:%d-%d" % (chrom, start, end))
-        # if len(this_st_depth) > 0:
-        #     this_depth = pd.read_csv(StringIO(this_st_depth), sep='\t', header=None)
-        #     this_depth.columns = ['chrom', 'coord', 'cov']
-        #     perm_depths[j] = this_depth
 
     return region_depth, perm_depths
 
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    required = parser.add_argument_group('required arguments')
+
+    required.add_argument("-i", "--input", help="Path to BAM file", required=True)
+    required.add_argument("-p", "--threads", help="Number of threads", default=os.environ.get('NSLOTS', '1'))
+
+    # all extra args got to extra
+    args, extra = parser.parse_known_args()
+
+    ncpu = int(args.threads)
+    f = os.path.abspath(args.input)
+
+    flogger.info("Running on input file %s with %d threads.", f, ncpu)
+
     outdir = output.unique_output_dir("rrbs_coverage_sampling", reuse_empty=True)
 
+    flogger.info("Output to directory %s", outdir)
+
     region_pad = 500  # number of BP to pad each region by
-    n_perm = 5  # number of times to draw each region size at random
+    n_perm = 10  # number of times to draw each region size at random
 
     bed_fn = os.path.join(GIT_LFS_DATA_DIR, 'mouse_cpg_island', 'grcm38_cpgisland.bed')
     tsv_fn = os.path.join(GIT_LFS_DATA_DIR, 'mouse_cpg_island', 'grcm38_cpgisland.tsv')
@@ -66,103 +70,68 @@ if __name__ == "__main__":
     chrom_lengths = chrom_lengths.loc[chrom_lengths.index.str.contains(r'^[0-9]')]
 
     # BAMs must be sorted and indexed
-    flist = [
-        os.path.join(indir, subdir.format(i=i), 'GC-CV-7163-{i}_S{i}_pe.sorted.bam'.format(i=i))
-        for i in range(1, 7)
-    ]
+    # flist = [
+    #     os.path.join(indir, subdir.format(i=i), 'GC-CV-7163-{i}_S{i}_pe.sorted.bam'.format(i=i))
+    #     for i in range(1, 7)
+    # ]
 
-    for f in flist:
-        fstem = os.path.split(f)[-1]
-        fstem = os.path.splitext(fstem)[0]
-        fstem = fstem.replace('.sorted', '')
-        fstem = re.sub(r'_pe$', '', fstem)
+    # for f in flist:
+    fstem = os.path.split(f)[-1]
+    fstem = os.path.splitext(fstem)[0]
+    fstem = fstem.replace('.sorted', '')
+    fstem = re.sub(r'_pe$', '', fstem)
 
-        fn = os.path.join(indir, f)
-        clogger.info("Starting analysis of %s.", fn)
-        s = pysam.AlignmentFile(fn, 'rb')
+    fn = os.path.join(indir, f)
+    flogger.info("Starting analysis of %s.", fn)
+    s = pysam.AlignmentFile(fn, 'rb')
 
-        # load tsv
-        cpg_regions = pd.read_csv(tsv_fn, sep='\t', header=0)
+    # load tsv
+    cpg_regions = pd.read_csv(tsv_fn, sep='\t', header=0)
 
-        # MP
-        if 'NSLOTS' in os.environ:
-            ncpu = int(os.environ['NSLOTS'])
-        else:
-            ncpu = mp.cpu_count()
-        clogger.info("Using %d parallel processes", ncpu)
-
+    if ncpu > 1:
         pool = mp.Pool(ncpu)
         jobs = {}
 
-        cov_cpg_islands = []
-        cov_perms = []
+    cov_cpg_islands = []
+    cov_perms = []
 
-        for i, row in cpg_regions.iterrows():
-            region = (row.chrom, row.chromStart, row.chromEnd)
+    for i, row in cpg_regions.iterrows():
+        region = (row.chrom, row.chromStart, row.chromEnd)
+        kwds = {'region_pad': region_pad, 'n_perm': n_perm}
+        if ncpu > 1:
             jobs[i] = pool.apply_async(
                 get_one_coverage,
                 args=(fn, region),
-                kwds={'region_pad': region_pad, 'n_perm': n_perm}
+                kwds=kwds
             )
+        else:
+            try:
+                res = get_one_coverage(fn, region, **kwds)
+                cov_cpg_islands.append(res[0])
+                cov_perms.append(res[1])
+            except Exception:
+                flogger.exception("Failed to extract region %s:%d-%d.", row.chrom, row.chromStart, row.chromEnd)
 
+    if ncpu > 1:
         pool.close()
-        # pool.join()
         for i, row in cpg_regions.iterrows():
-            ## DEBUG!
-            if i > 200:
-                break
             if (i % 1000) == 0:
-                clogger.info("Region %d / %d", i, cpg_regions.shape[0])
+                flogger.info("Region %d / %d", i, cpg_regions.shape[0])
             try:
                 res = jobs[i].get(1e6)
                 cov_cpg_islands.append(res[0])
                 cov_perms.append(res[1])
             except Exception:
-                clogger.exception("Failed to extract region %s:%d-%d.", row.chrom, row.chromStart, row.chromEnd)
+                flogger.exception("Failed to extract region %s:%d-%d.", row.chrom, row.chromStart, row.chromEnd)
 
-        # save results
-        to_write = {
-            'cpg_islands': cov_cpg_islands,
-            'permutations': cov_perms,
-        }
+    # save results
+    to_write = {
+        'cpg_islands': cov_cpg_islands,
+        'permutations': cov_perms,
+    }
 
-        outfn = os.path.join(outdir, "%s.cpg_coverage.json" % fstem)
-        clogger.info("Writing JSON results to %s", outfn)
-        with open(outfn, 'wb') as fout:
-            json.dump(to_write, fout)
-        clogger.info("Completed file %s.", fn)
-
-
-            # for each region, get reads
-    # for i, row in cpg_regions.iterrows():
-        # chrom = row.chrom
-        # start = max( - region_pad, 0)
-        # end = row.chromEnd + region_pad  # no problem if we run off the end
-        # # get the coverage over this region
-        # try:
-        #     this_st_depth = pysam.depth(fn, "-r", "%s:%d-%d" % (chrom, start, end))
-        #     if len(this_st_depth) > 0:
-        #         # more parsing is needed
-        #         this_depth = pd.read_csv(StringIO(this_st_depth), sep='\t', header=None)
-        #         this_depth.columns = ['chrom', 'coord', 'cov']
-        #         cov_cpg_islands.append(this_depth)
-        #     else:
-        #         cov_cpg_islands.append(0)
-        #
-        #     this_perms = []
-        #     for j in range(n_perm):
-        #         chrom, start, end = genomics.random_genomic_interval(chrom_lengths, n_bp=end - start + 1)
-        #         this_st_depth = pysam.depth(fn, "-r", "%s:%d-%d" % (chrom, start, end))
-        #         if len(this_st_depth) > 0:
-        #             this_depth = pd.read_csv(StringIO(this_st_depth), sep='\t', header=None)
-        #             this_depth.columns = ['chrom', 'coord', 'cov']
-        #             this_perms.append(this_depth)
-        #         else:
-        #             this_perms.append(0)
-        #     cov_perms.append(this_perms)
-        #
-        # except Exception as exc:
-        #     print "Failed to extract region %s:%d-%d - %s" % (
-        #         chrom, start, end, repr(exc)
-        #     )
-
+    outfn = os.path.join(outdir, "%s.cpg_coverage.json" % fstem)
+    flogger.info("Writing JSON results to %s", outfn)
+    with open(outfn, 'wb') as fout:
+        json.dump(to_write, fout)
+    flogger.info("Completed file %s.", fn)
