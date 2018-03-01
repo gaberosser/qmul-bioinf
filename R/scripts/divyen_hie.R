46c62dd4165707e39db5fc75be0a03e4543de96d
Gabriel Rosser <gabriel.rosser@gmail.com>
2018-03-01 22:23:37 +0000

    Added more GLMs to the DS R code

--- R/scripts/divyen_hie.R
+++ R/scripts/divyen_hie.R
@@ -50,8 +50,12 @@ y <- as.factor(y)
 values <- dat[, c(peak_cols, trough_cols)]
 values <- imputeMissing(values)
 
-X <- data.frame(values, outcome=y == 'Unfav')
+plt.given <- data.frame(Platelets=rep(F, nrow(X)), row.names = rownames(X))
+plt.given[grep('Platelets', dat$Blood.product), 'Platelets'] <- T
 
+X <- data.frame(values, plt.given, batch=batch, outcome=y == 'Unfav')
+
+# Initial model: all biomarkers, dept = outcome
 fit.glm <- glm(outcome ~ Hb.peak + Plt.peak + Neutrophil.peak + 
                  Lymphocyte.peak + INR.peak + Urea.peak + Creatinine.peak + 
                  ALT.peak + CRP.peak + APTT.peak + Plt.trough,
@@ -65,3 +69,50 @@ anova(fit.null, fit.glm, test="Chisq")
 # coefficients
 odds <- exp(coef(fit.glm))
 odds.ci <- exp(confint.default(fit.glm))
+
+# 2: Is the platelet product predictive of outcome?
+fit.glm.plt <- glm(outcome ~ Hb.peak + Plt.peak + Neutrophil.peak + 
+                 Lymphocyte.peak + INR.peak + Urea.peak + Creatinine.peak + 
+                 ALT.peak + CRP.peak + APTT.peak + Plt.trough + Platelets,
+               data=X)
+
+summary(fit.glm.plt)
+
+anova(fit.glm, fit.glm.plt, test="Chisq")
+
+# 3: Now let's load the full cohort
+fn.full <- file.path(data.dir, 'divyen_shah', 'cleaned_data_full_cohort_feb_2018.csv')
+dat.full <- read.csv(fn.full)
+
+y.full <- dat.full$Outcome
+y.full[y.full == 1] <- 'Unfav'
+y.full[y.full == 2] <- 'Fav'
+y.full <- as.factor(y.full)
+
+values.full <- dat.full[, c(peak_cols, trough_cols)]
+values.full <- imputeMissing(values.full)
+mec.or.cult <- dat.full$Meconium.Aspiration == 'Y'
+mec.or.cult[dat.full$Culture == 'Y'] <- T
+
+X.full <- data.frame(values.full, mec.or.cult=mec.or.cult, outcome=y.full == 'Unfav')
+
+fit.mec <- glm(mec.or.cult ~ Hb.peak + Plt.peak + Neutrophil.peak + 
+                 Lymphocyte.peak + INR.peak + Urea.peak + Creatinine.peak + 
+                 ALT.peak + CRP.peak + APTT.peak + Plt.trough, data = X.full)
+fit.mec.null <- glm(mec.or.cult ~ 1, data = X.full)
+
+anova(fit.mec.null, fit.mec, test='Chisq')
+
+odds.mec <- exp(coef(fit.mec))
+odds.ci.mec <- exp(confint.default(fit.mec))
+
+# conclusion: yes, the meconium or culture status is predictable from the biomarkers.
+# but can we still use these?
+
+fit.outcome.full <- glm(outcome ~ mec.or.cult + Hb.peak + Plt.peak + Neutrophil.peak + 
+                          Lymphocyte.peak + INR.peak + Urea.peak + Creatinine.peak + 
+                          ALT.peak + CRP.peak + APTT.peak + Plt.trough, data = X.full)
+
+fit.outcome.full.hier <- glm(outcome ~ mec.or.cult * (Hb.peak + Plt.peak + Neutrophil.peak + 
+                          Lymphocyte.peak + INR.peak + Urea.peak + Creatinine.peak + 
+                          ALT.peak + CRP.peak + APTT.peak + Plt.trough), data = X.full)
