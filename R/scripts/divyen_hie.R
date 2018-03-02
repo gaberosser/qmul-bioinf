b21a0e2c9f5f128e92fe8f9f2cdad7e8c759e3ed
Gabriel Rosser <gabriel.rosser@gmail.com>
2018-03-02 07:16:37 +0000

    Remove APTT, add anova calls

--- R/scripts/divyen_hie.R
+++ R/scripts/divyen_hie.R
@@ -25,18 +25,14 @@ biomarkers <- c(
 'Plt',
 'Neutrophil',
 'Lymphocyte',
-# 'PT',
+# 'PT',  # removed as it is highly correlated with INR
 'INR',
-'APTT',
+# 'APTT',  # removed as it is missing in one batch
 'Urea',
 'Creatinine',
 'ALT',
 'ALP',
 'CRP'
-)
-
-biomarkers.shortened <- c(
-  'Hb', 'Plt', 'Nphil', 'Lcyte', 'PT', 'INR', 'APTT', 'Urea', 'Cnine', 'ALT', 'ALP', 'CRP'
 )
 
 peak_cols <- paste(biomarkers, 'peak', sep='.')
@@ -50,7 +46,7 @@ y <- as.factor(y)
 values <- dat[, c(peak_cols, trough_cols)]
 values <- imputeMissing(values)
 
-plt.given <- data.frame(Platelets=rep(F, nrow(X)), row.names = rownames(X))
+plt.given <- data.frame(Platelets=rep(F, nrow(dat)), row.names = rownames(dat))
 plt.given[grep('Platelets', dat$Blood.product), 'Platelets'] <- T
 
 X <- data.frame(values, plt.given, batch=batch, outcome=y == 'Unfav')
@@ -58,24 +54,32 @@ X <- data.frame(values, plt.given, batch=batch, outcome=y == 'Unfav')
 # Initial model: all biomarkers, dept = outcome
 fit.glm <- glm(outcome ~ Hb.peak + Plt.peak + Neutrophil.peak + 
                  Lymphocyte.peak + INR.peak + Urea.peak + Creatinine.peak + 
-                 ALT.peak + CRP.peak + APTT.peak + Plt.trough,
-               data=X)
+                 ALT.peak + CRP.peak + Plt.trough,
+               data=X, family = binomial(link = "logit"))
 
 summary(fit.glm)
-fit.null <- glm(outcome ~ 1, data=X)
+# we can't trust the P values from the previous approach as they are based on the Wald test, so we should run a likelihood based test now.
+# https://stats.stackexchange.com/questions/315684/i-think-my-logistic-model-is-overfitted-even-with-lasso-r-gives-me-a-perfect-se?rq=1
+anova(fit.glm, test='Chi')
+
+fit.null <- glm(outcome ~ 1, data=X, family=binomial())
 
 anova(fit.null, fit.glm, test="Chisq")
+
+# fit a reduced model to improve overfitting
+fit.reduced <- glm(outcome ~ Plt.peak + Plt.trough, data=X, family=binomial())
+
 
 # coefficients
 odds <- exp(coef(fit.glm))
 odds.ci <- exp(confint.default(fit.glm))
+odds_table <- data.frame(odds=odds, odds.ci)
 
 # 2: Is the platelet product predictive of outcome?
 fit.glm.plt <- glm(outcome ~ Hb.peak + Plt.peak + Neutrophil.peak + 
                  Lymphocyte.peak + INR.peak + Urea.peak + Creatinine.peak + 
-                 ALT.peak + CRP.peak + APTT.peak + Plt.trough + Platelets,
-               data=X)
-
+                 ALT.peak + CRP.peak + Plt.trough + Platelets,
+               data=X, family=binomial())
 summary(fit.glm.plt)
 
 anova(fit.glm, fit.glm.plt, test="Chisq")
