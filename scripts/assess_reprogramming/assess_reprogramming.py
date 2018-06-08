b79afb50dfc49ba36a74e2de23c209e445fd26a5
Gabriel Rosser <g.rosser@qmul.ac.uk>
2018-06-08 17:24:45 +0100

    neatened up transcription based clustering and added gene signature work.

--- scripts/assess_reprogramming/assess_reprogramming.py
+++ scripts/assess_reprogramming/assess_reprogramming.py
@@ -6,6 +6,8 @@ import numpy as np
 import copy
 import os
 from utils import output
+import references
+from scipy.stats import zscore
 
 
 class SetMe(object):
@@ -29,10 +31,53 @@ def load_refs(ref_dict, **load_kwds):
     return loader.loader.MultipleBatchLoader(ref_objs_arr)
 
 
+def combine_filter(dat_arr, meta_arr, min_val=1, n_above_min=3):
+    dat = pd.concat(dat_arr, axis=1).dropna(axis=0)
+    meta = pd.concat(meta_arr, axis=0).loc[dat.columns]
+
+    # combine `cell type` and `type` fields
+    meta['type'].loc[meta['type'].isnull()] = meta['cell type'].loc[meta['type'].isnull()]
+
+    # merge the batch for our data
+    meta.loc[meta['batch'].str.contains('wtchg'), 'batch'] = 'WTCHG'
+
+    # filter and QN
+    dat = dat.loc[(dat > min_val).sum(axis=1) > n_above_min]
+
+    return dat, meta
+
+
+def construct_colour_array_legend_studies(meta):
+    studies = {}
+    cc = pd.DataFrame('gray', index=meta.index, columns=['Cell type', 'Study'])
+
+    cc.loc[meta['type'] == 'FB', 'Cell type'] = '#fff89e'
+    cc.loc[(meta['type'] == 'iPSC') & (meta['batch'].str.contains('WTCHG')), 'Cell type'] = 'blue'
+    cc.loc[(meta['type'] == 'iPSC') & (~meta['batch'].str.contains('WTCHG')), 'Cell type'] = '#96daff'
+    cc.loc[meta['type'] == 'ESC', 'Cell type'] = 'green'
+    cc.loc[meta['type'] == 'EPS', 'Cell type'] = '#7fc97f'
+    cc.loc[(meta['type'] == 'iNSC') & (meta['batch'].str.contains('WTCHG')), 'Cell type'] = '#9e3900'  # chestnut
+    cc.loc[meta['type'] == 'iNSC', 'Cell type'] = '#db7b00'  # orange
+    cc.loc[meta['type'] == 'NPC', 'Cell type'] = '#db7b00'  # orange
+    cc.loc[meta['type'] == 'NSC', 'Cell type'] = '#db7b00'  # orange
+    cc.loc[(meta['type'] == 'NSC') & (meta.index.str.contains('fetal')), 'Cell type'] = '#ffaf47'  # light orange
+
+    batches = meta.batch.unique()
+    n_study = len(batches)
+    study_colours = common.COLOUR_BREWERS[n_study]
+    for i, c in enumerate(study_colours):
+        cc.loc[meta['batch'] == batches[i], 'Study'] = c
+        studies[batches[i]] = c
+
+    return cc, studies
+
+
 if __name__ == '__main__':
     pids = ['017', '018', '019', '030', '031', '049', '050', '054', '061', '026', '052']
     min_val = 1
+    n_above_min = 3
     source = 'salmon'
+    n_hipsci = 30  # only plot a limited number to avoid flooding the plots
 
     ref_dict = {
         'GSE80732': {'batch': 'Yang et al.', 'strandedness': 'u', 'alignment_subdir': 'human/trimgalore'},
@@ -56,6 +101,10 @@ if __name__ == '__main__':
         'encode_roadmap/ENCSR977XUX': {'batch': 'ENCODE Ecker', 'strandedness': 'r'},
     }
 
+    # Ruiz 9 gene signature - should distinguish ESC and iPSC
+    gene_sign = ['PTPRT', 'TMEM132C', 'TMEM132D', 'TCERG1L', 'DPP6', 'FAM19A5', 'RBFOX1', 'CSMD1', 'C22orf34']
+    gene_sign_ens = references.gene_symbol_to_ensembl(gene_sign)
+
     outdir = output.unique_output_dir("assess_reprogramming")
 
     load_kwds = {'source': source, 'alignment_subdir': SetMe}
@@ -66,41 +115,50 @@ if __name__ == '__main__':
         # set strandedness as a cue to import for each
         load_kwds['strandedness'] = SetMe
 
+    # our data (everything)
     obj = loader.load_by_patient(pids, source=source)
-    dat_home = obj.data.loc[:, obj.meta.type.isin(['iPSC', 'FB'])]
 
     dat_hip, meta_hip = loader.hipsci_ipsc(aggregate_to_gene=True)
     meta_hip.insert(3, 'batch', 'HipSci')
 
+    # reduce the number in a (repeatably) random fashion
+    rs = np.random.RandomState(42)  # set the seed so we always get the same samples
+    idx = np.arange(dat_hip.shape[1])
+    rs.shuffle(idx)
+    dat_hip_reduced = dat_hip.iloc[:, idx[:n_hipsci]]
+    meta_hip_reduced = meta_hip.loc[dat_hip_reduced.columns]
+
     ref_obj = load_refs(ref_dict, **load_kwds)
-    dat_ref = ref_obj.data
+    dat_ref = ref_obj.data.copy()
+    meta_ref = ref_obj.meta.copy()
+
+    # our data - more selective
+    dat_home = obj.data.loc[:, obj.meta.type.isin(['iPSC', 'FB'])]
 
     # discard irrelevant samples
 
     to_discard = [
         '_rapamycin_',
+        'HFF_PD16', 'HFF_PD46', 'HFF_PD64', 'HFF_PD74',
+        'BJ_OLD',
+        'IMR90_O',
+        'MRC_5_PD52', 'MRC_5_PD62', 'MRC_5_PD72',
+        'WI_38_O',
     ]
     for td in to_discard:
         the_idx = ~dat_ref.columns.str.contains(td)
         dat_ref = dat_ref.loc[:, the_idx]
+        meta_ref = meta_ref.loc[the_idx]
 
-    dat = pd.concat((dat_home, dat_ref), axis=1).dropna(axis=0)
-    meta = pd.concat((obj.meta, ref_obj.meta), axis=0).loc[dat.columns]
-    meta['type'].loc[meta['type'].isnull()] = meta['cell type'].loc[meta['type'].isnull()]
-    # merge the batch for our data
-    meta.loc[meta['batch'].str.contains('wtchg'), 'batch'] = 'WTCHG'
+    dat, meta = combine_filter(
+        (dat_home, dat_ref),
+        (obj.meta, ref_obj.meta),
+        min_val=min_val,
+        n_above_min=n_above_min,
+    )
+    dat_qn = transformations.quantile_normalisation(np.log2(dat + 1))
 
-    dat = dat.loc[(dat > min_val).sum(axis=1) > 6]
-    # dat_qn = transformations.quantile_normalisation(dat)
-    dat_qn_log = transformations.quantile_normalisation(np.log2(dat + 1))
-
-    cc = pd.DataFrame('gray', index=dat.columns, columns=['Cell type', 'Study'])
-
-    cc.loc[meta['type'] == 'FB', 'Cell type'] = '#fff89e'
-    cc.loc[(meta['type'] == 'iPSC') & (meta['batch'].str.contains('WTCHG')), 'Cell type'] = 'blue'
-    cc.loc[(meta['type'] == 'iPSC') & (~meta['batch'].str.contains('WTCHG')), 'Cell type'] = '#96daff'
-    cc.loc[meta['type'] == 'ESC', 'Cell type'] = 'green'
-    cc.loc[meta['type'] == 'EPS', 'Cell type'] = '#7fc97f'
+    cc, st = construct_colour_array_legend_studies(meta)
 
     leg_dict = {
         'Cell type': {
@@ -110,105 +168,120 @@ if __name__ == '__main__':
             'ESC': 'green',
             'Enhanced PSC': '#7fc97f',
         },
-        'Study': {},
+        'Study': st,
     }
 
-    batches = meta.batch.unique()
-    n_study = len(batches)
-    study_colours = common.COLOUR_BREWERS[n_study]
-    for i, c in enumerate(study_colours):
-        cc.loc[meta['batch'] == batches[i], 'Study'] = c
-        leg_dict['Study'][batches[i]] = c
-
-    dend = clustering.dendrogram_with_colours(dat_qn_log, cc, vertical=True, legend_labels=leg_dict, fig_kws={'figsize': [14, 6]})
+    dend = clustering.dendrogram_with_colours(dat_qn, cc, vertical=True, legend_labels=leg_dict, fig_kws={'figsize': [14, 6]})
     dend['fig'].savefig(os.path.join(outdir, "cluster_ipsc_esc_fb.png"), dpi=200)
 
+    # Only iPSC and ESC
+
+    dat = dat.loc[:, meta.type != 'FB']
+    meta = meta.loc[dat.columns]
+    dat_qn = transformations.quantile_normalisation(np.log2(dat + 1))
+
+    cc, st = construct_colour_array_legend_studies(meta)
+
+    leg_dict = {
+        'Cell type': {
+            'iPSC (this study)': 'blue',
+            'iPSC': '#96daff',
+            'ESC': 'green',
+            'Enhanced PSC': '#7fc97f',
+        },
+        'Study': st,
+    }
+
+    dend = clustering.dendrogram_with_colours(dat_qn, cc, vertical=True, legend_labels=leg_dict, fig_kws={'figsize': [14, 6]})
+    dend['fig'].savefig(os.path.join(outdir, "cluster_ipsc_esc.png"), dpi=200)
+
+    # Ruiz signature (only)
+
+    dat_r_z = np.log2(dat + 1).loc[gene_sign_ens.values].dropna().apply(zscore, axis=1)
+    dat_r_z.index = gene_sign_ens.index[gene_sign_ens.isin(dat_r_z.index)]
+
+    cg = clustering.plot_clustermap(dat_r_z, show_gene_labels=True, cmap='RdBu_r')
+    cg.gs.update(bottom=0.2)
+    cg.savefig(os.path.join(outdir, "clustermap_ruiz_ipsc_esc_ztrans.png"), dpi=200)
+
     # now add some HiPSCi and repeat
-    n_hipsci = 50
-    rs = np.random.RandomState(42)
-    idx = np.arange(dat_hip.shape[1])
-    rs.shuffle(idx)
-    this_dat_hip = dat_hip.iloc[:, idx[:n_hipsci]]
-    this_meta_hip = meta_hip.loc[this_dat_hip.columns]
 
-    dat = pd.concat((dat_home, dat_ref, this_dat_hip), axis=1).dropna(axis=0)
-    meta = pd.concat((obj.meta, ref_obj.meta, this_meta_hip), axis=0).loc[dat.columns]
-    meta['type'].loc[meta['type'].isnull()] = meta['cell type'].loc[meta['type'].isnull()]
-    # merge the batch for our data
-    meta.loc[meta['batch'].str.contains('wtchg'), 'batch'] = 'WTCHG'
+    dat, meta = combine_filter(
+        (dat_home, dat_ref, dat_hip_reduced),
+        (obj.meta, ref_obj.meta, meta_hip_reduced),
+        min_val=min_val,
+        n_above_min=n_above_min,
+    )
+    dat_qn = transformations.quantile_normalisation(np.log2(dat + 1))
 
-    dat = dat.loc[(dat > min_val).sum(axis=1) > 6]
-    dat_qn_log = transformations.quantile_normalisation(np.log2(dat + 1))
-
-    cc = pd.DataFrame('gray', index=dat.columns, columns=['Cell type', 'Study'])
-
-    cc.loc[meta['type'] == 'FB', 'Cell type'] = '#fff89e'
-    cc.loc[(meta['type'] == 'iPSC') & (meta['batch'].str.contains('WTCHG')), 'Cell type'] = 'blue'
-    cc.loc[(meta['type'] == 'iPSC') & (~meta['batch'].str.contains('WTCHG')), 'Cell type'] = '#96daff'
-    cc.loc[meta['type'] == 'ESC', 'Cell type'] = 'green'
-    cc.loc[meta['type'] == 'EPS', 'Cell type'] = '#7fc97f'
-
-    batches = meta.batch.unique()
-    n_study = len(batches)
-    study_colours = common.COLOUR_BREWERS[n_study]
-    for i, c in enumerate(study_colours):
-        cc.loc[meta['batch'] == batches[i], 'Study'] = c
-        leg_dict['Study'][batches[i]] = c
+    cc, st = construct_colour_array_legend_studies(meta)
+    leg_dict = {
+        'Cell type': {
+            'FB': '#fff89e',
+            'iPSC (this study)': 'blue',
+            'iPSC': '#96daff',
+            'ESC': 'green',
+            'Enhanced PSC': '#7fc97f',
+        },
+        'Study': st,
+    }
 
     dend = clustering.dendrogram_with_colours(
-        dat_qn_log,
+        dat_qn,
         cc,
         vertical=True,
         legend_labels=leg_dict,
         fig_kws={'figsize': [14, 6]},
-        show_labels=False
+        # show_labels=False
     )
-
     dend['fig'].savefig(os.path.join(outdir, "cluster_ipsc_esc_fb_with_hipsci%d.png" % n_hipsci), dpi=200)
 
-    # Let's bring in our NSC and ref NSC now?
+    # remove FB and repeat
+    dat = dat.loc[:, meta.type != 'FB']
+    meta = meta.loc[dat.columns]
+    dat_qn = transformations.quantile_normalisation(np.log2(dat + 1))
 
-    # reload our data, because we want the iNSC and Gibco included
-    obj = loader.load_by_patient(pids, source=source)
+    cc, st = construct_colour_array_legend_studies(meta)
+
+    leg_dict = {
+        'Cell type': {
+            'iPSC (this study)': 'blue',
+            'iPSC': '#96daff',
+            'ESC': 'green',
+            'Enhanced PSC': '#7fc97f',
+        },
+        'Study': st,
+    }
+
+    dend = clustering.dendrogram_with_colours(dat_qn, cc, vertical=True, legend_labels=leg_dict, fig_kws={'figsize': [14, 6]})
+    dend['fig'].savefig(os.path.join(outdir, "cluster_ipsc_esc_with_hipsci%d.png" % n_hipsci), dpi=200)
+
+    # Ruiz signature (only)
+
+    dat_r_z = np.log2(dat + 1).loc[gene_sign_ens.values].dropna().apply(zscore, axis=1)
+    dat_r_z.index = gene_sign_ens.index[gene_sign_ens.isin(dat_r_z.index)]
+
+    cg = clustering.plot_clustermap(dat_r_z, show_gene_labels=True, cmap='RdBu_r', )
+    cg.gs.update(bottom=0.2)
+    cg.savefig(os.path.join(outdir, "clustermap_ruiz_ipsc_esc_with_hipsci%d.png" % n_hipsci), dpi=200)
+
+    # Let's bring in our NSC and ref NSC now
+
     dat_home = obj.data.loc[:, obj.meta.type.isin(['iPSC', 'FB', 'NSC', 'iNSC'])]
 
     # load additional NSC ref data, previously withheld
     nsc_ref_obj = load_refs(nsc_ref_dict, **load_kwds)
     dat_nsc_ref = nsc_ref_obj.data
 
-    dat = pd.concat((dat_home, dat_ref, dat_nsc_ref), axis=1).dropna(axis=0)
-    meta = pd.concat((obj.meta, ref_obj.meta, nsc_ref_obj.meta), axis=0).loc[dat.columns]
-    meta['type'].loc[meta['type'].isnull()] = meta['cell type'].loc[meta['type'].isnull()]
-    # merge the batch for our data
-    meta.loc[meta['batch'].str.contains('wtchg'), 'batch'] = 'WTCHG'
+    dat, meta = combine_filter(
+        (dat_home, dat_ref, dat_nsc_ref),
+        (obj.meta, ref_obj.meta, nsc_ref_obj.meta),
+        min_val=min_val,
+        n_above_min=n_above_min
+    )
+    dat_qn = transformations.quantile_normalisation(np.log2(dat + 1))
 
-    dat = dat.loc[(dat > min_val).sum(axis=1) > 6]
-    dat_qn_log = transformations.quantile_normalisation(np.log2(dat + 1))
-
-    leg_dict['Cell type'].update({
-        'iNSC (this study)': '#9e3900',
-        'iNSC': '#db7b00',
-        'Fetal NSC': '#ffaf47'
-    })
-
-    cc = pd.DataFrame('gray', index=dat.columns, columns=['Cell type', 'Study'])
-
-    cc.loc[meta['type'] == 'FB', 'Cell type'] = '#fff89e'
-    cc.loc[(meta['type'] == 'iPSC') & (meta['batch'].str.contains('WTCHG')), 'Cell type'] = 'blue'
-    cc.loc[(meta['type'] == 'iPSC') & (~meta['batch'].str.contains('WTCHG')), 'Cell type'] = '#96daff'
-    cc.loc[meta['type'] == 'ESC', 'Cell type'] = 'green'
-    cc.loc[meta['type'] == 'EPS', 'Cell type'] = '#7fc97f'
-    cc.loc[meta['type'] == 'fetal NSC', 'Cell type'] = '#ffaf47'  # light orange
-    cc.loc[(meta['type'] == 'iNSC') & (meta['batch'].str.contains('WTCHG')), 'Cell type'] = '#9e3900'  # chestnut
-    cc.loc[meta['type'] == 'NPC', 'Cell type'] = '#db7b00'  # orange
-    cc.loc[meta['type'] == 'NSC', 'Cell type'] = '#db7b00'  # orange
-
-    batches = meta.batch.unique()
-    n_study = len(batches)
-    study_colours = common.COLOUR_BREWERS[n_study]
-    for i, c in enumerate(study_colours):
-        cc.loc[meta['batch'] == batches[i], 'Study'] = c
-        leg_dict['Study'][batches[i]] = c
+    cc, st = construct_colour_array_legend_studies(meta)
 
     leg_dict = {
         'Cell type': {
@@ -217,19 +290,15 @@ if __name__ == '__main__':
             'iPSC': '#96daff',
             'ESC': 'green',
             'Enhanced PSC': '#7fc97f',
+            'iNSC (this study)': '#9e3900',
+            'iNSC': '#db7b00',
+            'Fetal NSC': '#ffaf47'
         },
-        'Study': {},
+        'Study': st,
     }
 
-    batches = meta.batch.unique()
-    n_study = len(batches)
-    study_colours = common.COLOUR_BREWERS[n_study]
-    for i, c in enumerate(study_colours):
-        cc.loc[meta['batch'] == batches[i], 'Study'] = c
-        leg_dict['Study'][batches[i]] = c
-
     dend = clustering.dendrogram_with_colours(
-        dat_qn_log,
+        dat_qn,
         cc,
         vertical=True,
         legend_labels=leg_dict,
@@ -238,3 +307,39 @@ if __name__ == '__main__':
     )
 
     dend['fig'].savefig(os.path.join(outdir, "cluster_ipsc_esc_fb_nsc.png"), dpi=200)
+
+    # all of that AND HipSci samples
+
+    dat, meta = combine_filter(
+        (dat_home, dat_ref, dat_nsc_ref, dat_hip_reduced),
+        (obj.meta, ref_obj.meta, nsc_ref_obj.meta, meta_hip_reduced),
+        min_val=min_val,
+        n_above_min=n_above_min
+    )
+    dat_qn = transformations.quantile_normalisation(np.log2(dat + 1))
+
+    cc, st = construct_colour_array_legend_studies(meta)
+
+    leg_dict = {
+        'Cell type': {
+            'FB': '#fff89e',
+            'iPSC (this study)': 'blue',
+            'iPSC': '#96daff',
+            'ESC': 'green',
+            'Enhanced PSC': '#7fc97f',
+            'iNSC (this study)': '#9e3900',
+            'iNSC': '#db7b00',
+            'Fetal NSC': '#ffaf47'
+        },
+        'Study': st,
+    }
+
+    dend = clustering.dendrogram_with_colours(
+        dat_qn,
+        cc,
+        vertical=True,
+        legend_labels=leg_dict,
+        fig_kws={'figsize': [16, 6]},
+    )
+
+    dend['fig'].savefig(os.path.join(outdir, "cluster_ipsc_esc_fb_nsc_hipsci%d.png" % n_hipsci), dpi=200)
