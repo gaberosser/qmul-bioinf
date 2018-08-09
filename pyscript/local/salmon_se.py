956a9f20a2184df0a76798cdc6c5d582eadecd28
Gabriel Rosser <gabriel.rosser@gmail.com>
2018-08-09 08:58:48 +0100

    Local pyscript versions for Salmon PE, SE

---
+++ pyscript/local/salmon_se.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import argparse
+import sys
+import math
+
+# add root of project dir to the path
+sys.path.append(os.path.dirname(__file__) + '/../../')
+from pyscript import salmon
+
+
+if __name__ == "__main__":
+    salmon.run_salmon("se_bash")
