b3b8ceddd60c3f4741b695cc2f576c4f8080863f
Gabriel Rosser <g.rosser@qmul.ac.uk>
2018-08-29 16:07:58 +0100

    Tidied up featurecounts pyscript and fixed array bug (now runs as single task)

--- pyscript/featurecounts.py
+++ pyscript/featurecounts.py
@@ -1,4 +1,3 @@
-import math
 from pyscript import jobs, sge
 
 
@@ -21,11 +20,13 @@ class FeatureCountsBase(jobs.Job):
 
     def core_command(self):
         cmd = super(FeatureCountsBase, self).core_command()
-        return "{cmd} {bams}".format(cmd=cmd, bams=self.params[1])
+        return "{cmd} {bams}".format(cmd=cmd, bams=self.params)
 
 
     def prepare_submission(self, *args, **kwargs):
         self.setup_params(self.args['read_dir'])
+        # force a single job (no array)
+        self.params = self.params[1]
 
 
 class FeatureCountsApocrita(FeatureCountsSgeRequirements, FeatureCountsBase, jobs.BamFileSingleRunMixin):
@@ -34,3 +35,47 @@ class FeatureCountsApocrita(FeatureCountsSgeRequirements, FeatureCountsBase, job
 
 class FeatureCountsBash(jobs.BashJobMixin, FeatureCountsBase, jobs.BamFileSingleRunMixin):
     pass
+
+
+def run(run_type):
+    import argparse
+    import sys
+    import os
+
+    if run_type == 'bash':
+        cls = FeatureCountsBash
+    elif run_type == 'apocrita':
+        cls = FeatureCountsApocrita
+    else:
+        raise NotImplementedError("Unrecognised run_type option: %s" % run_type)
+
+    parser = argparse.ArgumentParser()
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+
+    optional.add_argument("--read_dir", help="Directory containing reads", default='./')
+    optional.add_argument("-o", "--out_dir", help="Output directory")
+    optional.add_argument("-T", "--threads", help="Number of threads", default='1')
+    optional.add_argument("--include", help="List of filestems to include (comma separated)")
+    optional.add_argument("--exclude", help="List of filestems to exclude (comma separated)")
+
+    required.add_argument("-a", "--gtf", help="GTF annotation file", required=True)
+
+    # all extra args got to extra
+    args, extra = parser.parse_known_args()
+
+    if args.out_dir is None:
+        # if no output_dir specified, create one in the reads directory
+        args.out_dir = os.path.join(args.read_dir, 'featurecounts')
+        if not os.path.exists(args.out_dir):
+            os.makedirs(args.out_dir)
+        sys.stderr.write("Output directory not specified, using default: %s\n" % args.out_dir)
+
+    if args.include is not None:
+        args.include = args.include.split(',')
+    if args.exclude is not None:
+        args.exclude = args.exclude.split(',')
+
+    obj = cls(extra_args=extra, **args.__dict__)
+    obj.create_script()
+    obj.submit()
