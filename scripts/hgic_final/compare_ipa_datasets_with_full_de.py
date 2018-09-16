f28af6b9d4d674e2d0ae57e9f5860f2e12a3adf6
Gabriel Rosser <gabriel.rosser@gmail.com>
2018-09-16 06:37:44 +0100

    Modified the data outputs for the 2 strategies main script, esp for S2. TODO: migrate across to hgic_final module when complete. Modified wideform DE creation function, adding option to include null set. Modularised IPA comparison code so it can be run for other analyses. Ran on S1 patient-specific to test.

--- scripts/hgic_final/compare_ipa_datasets_with_full_de.py
+++ scripts/hgic_final/compare_ipa_datasets_with_full_de.py
@@ -3,8 +3,66 @@ import os
 import numpy as np
 
 
+def compare(curr_df, ipa_df):
+    if ipa_df.index.sort_values().equals(curr_df.index.sort_values()):
+        print "Indexes match"
+    else:
+        print "Indexes DO NOT match"
+        ix = ipa_df.index.difference(curr_df.index)
+        print "In IPA dataset and not in current (%d): %s" % (
+            len(ix),
+            ', '.join(ix)
+        )
+        ix = curr_df.index.difference(ipa_df.index)
+        print "In current and not in IPA dataset (%d): %s" % (
+            len(ix),
+            ', '.join(ix)
+        )
+
+    # check remaining values agree
+    df2 = curr_df.reindex(ipa_df.index)
+    b = np.isclose(df2["%s_logFC" % p], df["%s_logFC" % p])
+    if b.all():
+        print "The logFC values are close in all cases."
+    else:
+        print "The logFC values are not close in %d cases." % (~b).sum()
+    b = np.isclose(np.log10(df2["%s_FDR" % p]), np.log10(df["%s_FDR" % p]))
+    if b.all():
+        print "The FDR values are close in all cases."
+    else:
+        print "The FDR values are not close in %d cases." % (~b).sum()
+
+
 if __name__ == '__main__':
     pids = ['018', '019', '030', '031', '017', '050', '054', '061', '026', '052']
+    # # full DE:
+    # ipa_indir = os.path.join(
+    #     os.path.expanduser('~'),
+    #     'Dropbox',
+    #     'research',
+    #     'qmul',
+    #     'hGIC_project',
+    #     'current',
+    #     'core_pipeline',
+    #     'rnaseq',
+    #     'ipa',
+    #     'data_uploaded'
+    # )
+    # # path to most current full DE results:
+    # curr_fn = os.path.join(
+    #     os.path.expanduser('~'),
+    #     'Dropbox',
+    #     'research',
+    #     'qmul',
+    #     'hGIC_project',
+    #     'current',
+    #     'core_pipeline',
+    #     'rnaseq',
+    #     'full_de.xlsx'
+    # )
+    # filestem = 'full_de_patient{pid}.txt'
+
+    # patient specific (S1):
     ipa_indir = os.path.join(
         os.path.expanduser('~'),
         'Dropbox',
@@ -14,10 +72,11 @@ if __name__ == '__main__':
         'current',
         'core_pipeline',
         'rnaseq',
+        's1_inter_patient',
         'ipa',
         'data_uploaded'
     )
-    # path to most current DE results
+    # path to most current S1 patient specific results:
     curr_fn = os.path.join(
         os.path.expanduser('~'),
         'Dropbox',
@@ -27,11 +86,15 @@ if __name__ == '__main__':
         'current',
         'core_pipeline',
         'rnaseq',
-        'full_de.xlsx'
+        's1_inter_patient',
+        'patient_specific_de.xlsx'
     )
+    filestem = 'patient_specific_de_{pid}.txt'
+
+
     full_de = pd.read_excel(curr_fn)
     for p in pids:
-        this_fn = os.path.join(ipa_indir, 'full_de_patient%s.txt' % p)
+        this_fn = os.path.join(ipa_indir, filestem.format(pid=p))
         df = pd.read_csv(this_fn, sep='\t', header=0, index_col=0).sort_values(by='%s_FDR' % p)
         this_full_de = full_de.loc[
             full_de[p] == 'Y',
@@ -39,31 +102,5 @@ if __name__ == '__main__':
         ].sort_values(by='%s_FDR' % p)
 
         print p
+        compare(this_full_de, df)
 
-        if df.index.sort_values().equals(this_full_de.index.sort_values()):
-            print "Indexes match"
-        else:
-            print "Indexes DO NOT match"
-            ix = df.index.difference(this_full_de.index)
-            print "In IPA dataset and not in current (%d): %s" % (
-                len(ix),
-                ', '.join(ix)
-            )
-            ix = this_full_de.index.difference(df.index)
-            print "In current and not in IPA dataset (%d): %s" % (
-                len(ix),
-                ', '.join(ix)
-            )
-
-        # check remaining values agree
-        df2 = this_full_de.reindex(df.index)
-        b = np.isclose(df2["%s_logFC" % p], df["%s_logFC" % p])
-        if b.all():
-            print "The logFC values are close in all cases."
-        else:
-            print "The logFC values are not close in %d cases." % (~b).sum()
-        b = np.isclose(np.log10(df2["%s_FDR" % p]), np.log10(df["%s_FDR" % p]))
-        if b.all():
-            print "The FDR values are close in all cases."
-        else:
-            print "The FDR values are not close in %d cases." % (~b).sum()
---
+++ scripts/hgic_final/prepare_data_for_gsea.py
@@ -0,0 +1,171 @@
+from rnaseq import loader, general
+from utils.output import unique_output_dir
+from rnaseq import gsea
+import os
+import consts
+import pandas as pd
+
+
+def ens_index_to_gene_symbol(df):
+    general.add_gene_symbols_to_ensembl_data(df)
+    tmp = df['Gene Symbol'].dropna()
+    df = df.loc[tmp.index]
+    df.set_index('Gene Symbol', inplace=True)
+    return df
+
+
+if __name__ == '__main__':
+    source = 'salmon'  # other options are 'star' (counts) and 'fpkm' (star/cufflinks)
+
+    outdir = unique_output_dir()
+
+    # load all data
+    pids = consts.PIDS
+    units = 'tpm'
+
+    out_subdir = os.path.join(outdir, units)
+    if not os.path.isdir(out_subdir):
+        os.makedirs(out_subdir)
+        print "Created output subdirectory %s" % out_subdir
+
+    source_by_units = {
+        'tpm': 'salmon',
+        'counts': 'star',
+        'fpkm': 'star/cufflinks'
+    }
+
+    obj = loader.load_by_patient(pids, source=source_by_units[units], include_control=True)
+
+    # set gibco aside
+    dat_gibco = obj.data.loc[:, obj.data.columns.str.contains('GIBCO')]
+    dat_gibco = ens_index_to_gene_symbol(dat_gibco)
+
+    # drop any cell types other than GBM and iNSC
+    ix = obj.meta['type'].isin(['GBM', 'iNSC'])
+    # drop unneeded GBM061 samples
+    ix = ix & (~obj.meta.index.isin(['DURA061_NSC_N1_P5', 'DURA061_NSC_N6_P4']))
+    obj.filter_samples(ix)
+
+    # convert to gene symbols
+    dat = ens_index_to_gene_symbol(obj.data)
+
+    # load reference dataset(s)
+    ref_obj = loader.load_references('GSE61794', strandedness='u', source=source_by_units[units])
+    ix = ref_obj.meta.index.str.contains('NSC')
+    ref_obj.filter_samples(ix)
+
+    # convert to gene symbols
+    dat_h9 = ens_index_to_gene_symbol(ref_obj.data)
+
+    # write single patient syngeneic comparison data
+    for pid in pids:
+        the_idx = dat.columns.str.contains(pid)
+        the_dat = dat.loc[:, the_idx]
+        the_classes = pd.Series('GBM', index=the_dat.columns)
+        the_classes.loc[the_classes.index.str.contains('NSC')] = 'iNSC'
+        out_fn = os.path.join(out_subdir, "%s.{ext}" % (pid))
+        gsea.data_to_gct(the_dat, out_fn.format(ext='gct'))
+        gsea.phenotypes_to_cls(the_classes, out_fn.format(ext='cls'))
+
+    # params
+    for pid in pids:
+        out_fn = os.path.join(out_subdir, "%s.{ext}" % (pid))
+        gsea.create_gsea_params_file(out_fn.format(ext='params'), rpt_label=pid)
+
+    # write single patient reference comparison data
+    refs = {
+        'gibco_nsc': dat_gibco,
+        'h9_nsc': dat_h9
+    }
+    for pid in pids:
+        ix = dat.columns.str.contains(pid) & dat.columns.str.contains('GBM')
+        n_ix = ix.sum()
+        for rnm, rd in refs.items():
+            the_dat = pd.concat(
+                (dat.loc[:, ix], rd),
+                axis=1
+            )
+            the_classes = pd.Series(['GBM'] * n_ix + ['NSC'] * rd.shape[1], index=the_dat.columns)
+            out_fn = os.path.join(out_subdir, "%s_%s.{ext}" % (pid, rnm))
+            gsea.data_to_gct(the_dat, out_fn.format(ext='gct'))
+            gsea.phenotypes_to_cls(the_classes, out_fn.format(ext='cls'))
+
+    # params
+    for pid in pids:
+        for rnm, rd in refs.items():
+            out_fn = os.path.join(out_subdir, "%s_%s.{ext}" % (pid, rnm))
+            gsea.create_gsea_params_file(out_fn.format(ext='params'), rpt_label="%s_%s" % (pid, rnm))
+
+    """
+    At this point, we need to run GSEA.
+
+    ##############################################
+    # 1. Running directly from the command line: #
+    ##############################################
+
+    java -Xmx16192m -cp /opt/gsea/gsea-3.0.jar xtools.gsea.Gsea -res /home/gabriel/Dropbox/research/qmul/results/gbm_insc_paired_sample_de/all/n_equals_2/gsea/tpm_salmon/rtkii.gct -cls /home/gabriel/Dropbox/research/qmul/results/gbm_insc_paired_sample_de/all/n_equals_2/gsea/tpm_salmon/rtkii.cls#GBM_versus_iNSC -gmx /home/gabriel/Dropbox/research/qmul/results/gbm_insc_paired_sample_de/all/n_equals_2/gsea/msigdb.v6.1.symbols.gmt -collapse false -norm meandiv -nperm 1000 -permute gene_set -rnd_type no_balance -scoring_scheme weighted -rpt_label RTK_II -metric Signal2Noise -sort real -order descending -create_gcts false -create_svgs false -include_only_symbols true -make_sets true -median false -num 1000 -plot_top_x 20 -rnd_seed timestamp -save_rnd_lists false -set_max 500 -set_min 15 -zip_report false -out /home/gabriel/gsea_home/output/rtk_II -gui false
+
+    #############################
+    # 2. Use a parameters file: #
+    #############################
+
+    # See rnaseq.gsea.create_gsea_params_file.
+    # Specify with -params_file
+
+    for i in *.params;
+        do b=$(basename $i .params);
+        if [[ $b = *"nsc"* ]]; then
+            c="#GBM_versus_NSC";
+        else
+            c="#GBM_versus_iNSC";
+        fi;
+        java -Xmx16192m -cp /opt/gsea/gsea-3.0.jar xtools.gsea.Gsea \
+        -res ${b}.gct -cls ${b}.cls${c} -gmx $gmx \
+        -out ./${b} \
+        -param_file ${b}.params
+
+        STATUS=$?
+
+        if [ $STATUS == 0 ]; then
+            echo "SUCCESS: ${b}"
+            mv ${b}.gct ${b}.cls ${b}.params ${b}/
+        else
+            echo "FAILED: ${b}"
+        fi
+    done
+
+    #######################################
+    # 3. parallel method for many files:  #
+    #######################################
+
+    runGsea() {
+        # how do I make this globally accessible within parallel? export it?
+        gmx="/home/gabriel/Dropbox/research/qmul/results/gbm_insc_paired_sample_de/all/n_equals_2/gsea/msigdb.v6.1.symbols.gmt"
+        b=$(basename $1 .params)
+
+        if [[ $b = *"nsc"* ]]; then
+            c="#GBM_versus_NSC";
+        else
+            c="#GBM_versus_iNSC";
+        fi;
+
+        java -Xmx16192m -cp /opt/gsea/gsea-3.0.jar xtools.gsea.Gsea \
+        -res ${b}.gct -cls ${b}.cls${c} -gmx $gmx \
+        -out ./${b} \
+        -param_file ${b}.params
+
+        STATUS=$?
+
+        if [ $STATUS == 0 ]; then
+            echo "SUCCESS: ${b}"
+            mv ${b}.gct ${b}.cls ${b}.params ${b}/
+        else
+            echo "FAILED: ${b}"
+        fi
+    }
+    # export so that the parallel env has the function in scope
+    export -f runGsea
+
+    parallel -j 3 runGsea ::: *.params
+
+    """
