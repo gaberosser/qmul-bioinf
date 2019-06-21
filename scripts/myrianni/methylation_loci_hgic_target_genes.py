be1759c3f73785896e9a2366c7734c41a3d137b5
Gabriel Rosser <g.rosser@qmul.ac.uk>
2019-06-21 16:44:39 +0100

    Add DE to detailed meth plot. Add new script to carry out TM analysis (just loading at the moment)

--- scripts/myrianni/methylation_loci_hgic_target_genes.py
+++ scripts/myrianni/methylation_loci_hgic_target_genes.py
@@ -9,11 +9,13 @@ import os
 from matplotlib import pyplot as plt
 import pysam
 import pandas as pd
+import pickle
 
 from utils import output, genomics, log, setops
 from plotting import genomics as genomic_plots
 from scripts.hgic_final import consts, two_strategies_grouped_dispersion as tsgd
 from methylation import loader as methylation_loader, dmr, process
+from rnaseq import loader as rnaseq_loader
 
 
 logger = log.get_console_logger()
@@ -53,7 +55,10 @@ if __name__ == '__main__':
 
     norm_method_s1 = 'swan'
     dmr_params = consts.DMR_PARAMS
+    de_params = consts.DE_PARAMS
+
     DMR_LOAD_DIR = os.path.join(output.OUTPUT_DIR, 'dmr')
+    DE_LOAD_DIR = os.path.join(output.OUTPUT_DIR, 'de')
 
     anno = methylation_loader.load_illumina_methylationepic_annotation()
 
@@ -71,6 +76,31 @@ if __name__ == '__main__':
         dmr_res_s1 = dmr.DmrResultCollection.from_pickle(fn, anno=anno)
     else:
         raise Exception("Unable to locate pre-existing results.")
+
+    # load DE results
+    rnaseq_obj = rnaseq_loader.load_by_patient(consts.PIDS, include_control=False)
+    rnaseq_obj.filter_by_sample_name(consts.S1_RNASEQ_SAMPLES)
+
+    the_hash = tsgd.de_results_hash(rnaseq_obj.meta.index.tolist(), de_params)
+    filename = 'de_results_paired_comparison.%d.pkl' % the_hash
+    fn = os.path.join(DE_LOAD_DIR, filename)
+
+    if os.path.isfile(fn):
+        logger.info("Reading S1 DE results from %s", fn)
+        with open(fn, 'rb') as f:
+            de_res_s1 = pickle.load(f)
+    else:
+        raise Exception("Unable to locate pre-existing DE results.")
+
+    the_hash = tsgd.de_results_hash(meth_obj.meta.index.tolist(), dmr_hash_dict)
+    filename = 'de_results_paired_comparison.%d.pkl' % the_hash
+    fn = os.path.join(DE_LOAD_DIR, filename)
+
+    if os.path.isfile(fn):
+        logger.info("Loading pre-computed DMR results from %s", fn)
+        dmr_res_s1 = dmr.DmrResultCollection.from_pickle(fn, anno=anno)
+    else:
+        raise Exception("Unable to locate pre-existing DMR results.")
 
     gois = ['PTGER4', 'NTRK2']
     rois = {
@@ -113,15 +143,20 @@ if __name__ == '__main__':
                     this_dmrs.setdefault(k, {})[pid] = t
 
         nrows = len(consts.PIDS) + 1  # one ax for each patient plus one for the track
-        gs = plt.GridSpec(nrows=nrows, ncols=1)
+        # first column shows methylation data, second RNA-Seq DE
+        gs = plt.GridSpec(nrows=nrows, ncols=2, width_ratios=(10, 1))
         fig = plt.figure(figsize=(8, 6))
-        track_ax = fig.add_subplot(gs[-1])
+        track_ax = fig.add_subplot(gs[-1, 0])
         sharex = None
+        de_share = None
         genomic_plots.plot_gene_transcripts(feat_res, ax=track_ax)
         dm_axs = {}
+        de_axs = {}
         for i, pid in enumerate(consts.PIDS):
-            dm_axs[pid] = fig.add_subplot(gs[i], sharex=sharex)
+            dm_axs[pid] = fig.add_subplot(gs[i, 0], sharex=sharex)
             sharex = dm_axs[pid]
+            de_axs[pid] = fig.add_subplot(gs[i, 1], sharex=de_share, sharey=de_share)
+            de_share = de_axs[pid]
 
         # keep tabs on all probes involved in DMRs (they may be outside of the region)
         probes_in_dmrs = set()
@@ -170,11 +205,33 @@ if __name__ == '__main__':
                 ymax = max(x.values.max(), ymax)
                 this_ax.set_ylabel(pid)
 
+            this_de_ax = de_axs[pid]
+            # bar chart to represent DE
+            ix = de_res_s1[pid]['Gene Symbol'] == g
+            if ix.sum() == 1:
+                this_de = de_res_s1[pid].loc[ix].squeeze()
+                this_de_ax.barh(
+                    [0],
+                    [this_de.logFC],
+                    edgecolor='k' if this_de.FDR < de_params['fdr'] else 'none',
+                    linewidth=1.,
+                    color=consts.METHYLATION_DIRECTION_COLOURS['hyper'] if this_de.logFC > 0 else consts.METHYLATION_DIRECTION_COLOURS['hypo']
+                )
+            elif ix.sum() > 1:
+                logger.warn("Gene %s. Found %d matching DE results in patient %s??", g, int(ix.sum()), pid)
+            else:
+                logger.warn("Gene %s. No matching DE result for patient %s.", g, pid)
+
+
         dm_axs[consts.PIDS[-1]].xaxis.set_ticklabels([])
+        plt.setp([t.yaxis for t in de_axs.values()], visible=False)
+        de_axs[consts.PIDS[-1]].set_ylim([-1, 1])
+        plt.setp(de_axs[consts.PIDS[-1]].xaxis.get_ticklabels(), rotation=90)
+        de_axs[consts.PIDS[-1]].set_xlabel('DE logFC')
 
         [ax.set_ylim([ymin * 1.05, ymax * 1.05]) for ax in dm_axs.values()]
 
-        gs.update(top=0.98, left=0.07, right=0.97, bottom=0.09, hspace=0.15)
+        gs.update(top=0.98, left=0.07, right=0.97, bottom=0.09, hspace=0.15, wspace=0.05)
         fig.savefig(os.path.join(outdir, "%s_mvalues.png" % g), dpi=300)
 
         if g in rois:
