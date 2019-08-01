6322a96e45a16171d9b52093c98161d9cd04e269
Gabriel Rosser <gabriel.rosser@gmail.com>
2019-08-01 12:59:58 +0100

    And added the callable scripts too

---
+++ pyscript/apocrita/cat_bams_barts_multilane.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import argparse
+import sys
+import math
+
+# add root of project dir to the path
+sys.path.append(os.path.dirname(__file__) + '/../../')
+from pyscript import cat_bams
+
+
+if __name__ == "__main__":
+    cat_bams.run("apocrita_multilane_barts")
